42c997d69824d7299b4deb44e34a2a4d1e344132
zenit <zenit@LAPTOP-I559NUA0>
2022-08-20 21:11:26 +0300

    Add Method protection App from a bug

---
+++ ReportExcel/ReportExcel/Class/Close.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace ReportExcel.Class
+{
+    class Close
+    {
+        /// <summary>
+        /// <para>Метод, реализующий защиту от дурака. Закрывает все файлы Excel,
+        /// чтобы можно изменить отчет</para>
+        /// <para>Method protection from a fool. Close all Excel file that it can update report</para>
+        /// </summary>
+        internal static void CloseProcess()
+        {
+            Process[] List;
+            List = Process.GetProcessesByName("EXCEL");
+            foreach (Process proc in List)
+            {
+                proc.Kill();
+            }
+        }
+    }
+}
+
---
+++ ReportExcel/ReportExcel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using ReportExcel.Class;
+namespace ReportExcel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Close.CloseProcess();
+        }
+    }
+}
